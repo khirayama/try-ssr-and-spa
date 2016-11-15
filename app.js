3db9881208a2027c4ab296ab7bcc03a6f2a0011f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-16 06:57:10 +0900

    Success SSR

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express');
+import express from 'express';
+
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+
+import App from './views/components/app';
 
 const app = express();
 
+function layout(content) {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>SSR + SPA</title>
+      </head>
+      <body>${content}</body>
+    </html>
+  `;
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  const content = renderToString(<App />);
+
+  res.status(200).send(layout(content));
 });
 
 app.listen(3000, () => {
